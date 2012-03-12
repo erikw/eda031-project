918a32a2e9987ba5b6d7a033916a6dd071779ece
Oscar Olsson <osse.olsson@gmail.com>
2012-03-12 14:57:22 +0100

    Headers MsgHndl, Query, Result

---
+++ net/messagehandler.h
@@ -0,0 +1,23 @@
+
+#ifndef MESSAGEHANDLER_H
+#define MESSAGEHANDLER_H
+
+#include "connection.h"
+#include "result.h"
+#include "query.h"
+
+namespace {
+	
+	class MessageHandler {
+		public:
+			MessageHandler(Connection& con);
+			Query& recieve_message();
+			void send_result(Result& res);
+		private:
+			Connection con;
+	};
+}
+
+
+
+#endif
---
+++ net/result.h
@@ -0,0 +1,13 @@
+
+#ifndef RESULT_H
+#define RESULT_H
+
+namespace net {
+	class Result{
+		public:
+			Result();
+			virtual printToConnection(Connection& conn);
+	}
+}
+
+#endif
