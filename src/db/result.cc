f3d14d6025fcd2d181ab8f2062af3c0a243fec30
Oscar Olsson <osse.olsson@gmail.com>
2012-03-15 18:33:07 +0100

    Impl common methods for writing to connection

---
+++ src/db/result.cc
@@ -0,0 +1,23 @@
+#include "result.h"
+#include "protocol.h"
+
+namespace db {
+	using namespace std;
+	using namespace net;
+
+	void Result::print_int(Connection& con, const int& num) throw(ConnectionClosedException){
+		con.write(Protocol::PAR_NUM);
+		con.write((num >> 24) && 0xFF);
+		con.write((num >> 16) && 0xFF);
+		con.write((num >> 8) && 0xFF);
+		con.write(num && 0xFF);
+	}
+
+	void Result::print_string(Connection& con, const string& str) throw(ConnectionClosedException){
+		con.write(Protocol::PAR_STRING);
+		print_int(con, str.size());
+		for(size_t i = 0; i < str.size(); ++i){
+			con.write(str[i]);
+		}
+	}
+}
--- src/db/result.h
+++ src/db/result.h
@@ -2,12 +2,15 @@
 #define RESULT_H
 #include "connection.h"
 #include "connectionclosedexception.h"
+#include <string>
 
 namespace db {
 	class Result{
 	public:
 		Result();
 		virtual void printToConnection(net::Connection& conn) throw(ConnectionClodesException)= 0;
+		void print_int(net::Connection& con, const int& num) throw(net::ConnectionClosedException);
+		void print_string(net::Connection& con, const std::string& str) throw(net::ConnectionClosedException);
 	};
 }
 
