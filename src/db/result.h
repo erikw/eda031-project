fcfeca46c79f0c5140362d1c8ae1e809beda7ec2
Oscar Olsson <osse.olsson@gmail.com>
2012-03-18 21:23:23 +0100

    added tests for db, fixed some errors.

--- src/db/result.h
+++ src/db/result.h
@@ -7,8 +7,8 @@
 namespace db {
 	class Result {
 	public:
-		void print_num(net::Connection &con, const int &num) throw(net::ConnectionClosedException);
 		virtual void printToConnection(net::Connection &conn) throw(net::ConnectionClosedException) = 0;
+		void print_num(net::Connection &con, const int &num) throw(net::ConnectionClosedException);
 		void print_string(net::Connection &con, const std::string &str) throw(net::ConnectionClosedException);
 	private:
 		void print_int(net::Connection &con, const int &num) throw(net::ConnectionClosedException);
