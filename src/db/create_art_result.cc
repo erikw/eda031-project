5ddc669fc42d4190233b58c7bcfec083b3fbfa42
Joachim Nelson <pimpimmi@gmail.com>
2012-03-19 12:14:17 +0100

    all results implemented

--- src/db/create_art_result.cc
+++ src/db/create_art_result.cc
@@ -2,8 +2,14 @@
 #include "net/connectionclosedexception.h"
 #include "net/connection.h"
 
+using namespace net;
+
 namespace db {
-	void CreateArtResult::printToConnection(net::Connection &conn) throw(net::ConnectionClosedException) {
-		;
+	void CreateArtResult::printToConnection(Connection &conn) throw(ConnectionClosedException) {
+		conn.write(Protocol::ANS_CREATE_ART);		
+		if (message != Protocol::ANS_ACK)
+			conn.write(Protocol::ANS_NAK);
+		conn.write(message);
+		conn.write(Protocol::ANS_END);
 	}
 }
