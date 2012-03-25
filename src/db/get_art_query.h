61f16d0d542050f73b723bb3f7c2d84f08d3f169
Erik Westrup <erik.westrup@gmail.com>
2012-03-25 17:45:02 +0200

    Deleted some ^Ms.

--- src/db/get_art_query.h
+++ src/db/get_art_query.h
@@ -9,7 +9,7 @@ namespace db {
 	class GetArtQuery : public Query {
 	public:
 		GetArtQuery(int ng_id, int art_id) : newsgroup_id(ng_id), article_id(art_id) {}
-        Result *getResult(Database &db);
+		Result *getResult(Database &db);
 		void send(net::MessageHandler& mh);
 	private:
 		int newsgroup_id;
